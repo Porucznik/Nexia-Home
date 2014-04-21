71debbd66910ebbc19b8845175609c5f7395d754
Porucznik <porucznik13@gmail.com>
2014-04-21 19:26:51 +0200

    Nexia Home

--- root-image/etc/haslo.rb
+++ root-image/etc/haslo.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-system "passwd arch"
+system "passwd nexia"
--- root-image/root/customize_root_image.sh
+++ root-image/root/customize_root_image.sh
@@ -2,7 +2,7 @@
 
 set -e -u
 
-sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(pl_PL\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
@@ -10,11 +10,16 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 usermod -s /bin/zsh root
 cp -aT /etc/skel/ /root/
 
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh arch
+useradd -m -p "" -g users -G "users,lp,video,adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh nexia
 
+chown -R root:root /etc/sudoers.d/
 chmod 750 /etc/sudoers.d
 chmod 440 /etc/sudoers.d/g_wheel
 
+sed -i 's/#\(\ %wheel\ ALL=(ALL)\ NOPASSWD:\ ALL\)/\1/' /etc/sudoers
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 
 systemctl enable multi-user.target pacman-init.service
+systemctl disable dhcpcd@
+systemctl enable wpa_supplicant
+systemctl enable NetworkManager
